f9aefbacebceee6277033d9facbda1b1a7fac8a9
sankarmangattu <sankarmangattu2@gmail.com>
2020-01-24 12:50:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-87gn3e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-87gn3e)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Asus Rog II',
+    price: 33999,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'redmi K20 Pro',
+    price: 24999,
+    description: 'A great phone with one of the best processor'
+  },
+  {
+    name: 'Samsung S10 Plus',
+    price: 79999,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
